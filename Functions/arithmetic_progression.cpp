3e3469e649f9358f29eb50506bef6172eafa96ee
themanjeetkr <themanjeetkr@gmail.com>
2024-10-04 15:32:17 +0530

    arithmetic_progression

---
+++ Functions/arithmetic_progression.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int a__(int a){
+    int nth=3*a+7;
+    return nth;
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int c=a__(n);
+    cout<<"the value of ap is:"<<c;
+
+
+
+}

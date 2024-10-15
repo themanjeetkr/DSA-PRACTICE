59926809f58886b09a9dc8d579f6a303f25a2094
themanjeetkr <themanjeetkr@gmail.com>
2024-10-15 06:43:07 +0530

    argu

--- Arrays/array_function.cpp
+++ Arrays/array_function.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+// syntax of argument
 void printarray(int arr[16],int size){
     cout<<"print the array"<<endl;
     for (int i = 0; i < size; i++)
@@ -17,8 +18,8 @@ int main(){
 int arr[16]={1};
 int n=10;
 
-
-printarray(arr, 16);
+// syntax of call function 
+printarray(arr, 16);  
 
 
 }

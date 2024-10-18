afdf08d44ff53342348cc3bf360504ac865491df
themanjeetkr <themanjeetkr@gmail.com>
2024-10-18 06:37:52 +0530

    swap

---
+++ Arrays/swap_array.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+void swap(int arr[], int size){
+for (int i = 0; i < size; i+=2)
+{
+    if (i+1 <size)
+    {
+    swap(arr[i], arr[i+1]);
+        
+    }
+    
+    
+}
+for (int i = 0; i < size; i++)
+{
+  cout<<arr[i]<<endl;
+}
+}
+
+
+
+
+
+int main(){
+    int even[8]={2,3,21,23,43,23,23,23};
+    swap(even,8);
+
+}

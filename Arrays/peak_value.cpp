b4236651c2c1b4f469c59367f9af94cb20e31605
themanjeetkr <themanjeetkr@gmail.com>
2024-10-27 20:12:55 +0530

    peak_value

---
+++ Arrays/peak_value.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int peak_value(int arr[], int n) {
+    int s = 0;
+    int e = n - 1; // Initialize 'e' to the last index
+
+    while (s < e) {
+        int mid = s + (e - s) / 2; // Calculate mid index
+
+        if (arr[mid] < arr[mid + 1]) {
+            s = mid + 1; // Move to the right half
+        } else {
+            e = mid; // Move to the left half
+        }
+    }
+
+    return arr[s]; // Return the peak value
+}
+
+int main() {
+    int arr[5] = {4, 5, 6, 1, 2};
+    int peakvalue = peak_value(arr, 5);
+    
+    cout << "Peak value: " << peakvalue << endl; // Print the peak value
+    return 0;
+}

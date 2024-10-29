f11a25fcfd96c083eef6001f99c68a2f587be1a2
themanjeetkr <themanjeetkr@gmail.com>
2024-10-29 20:52:04 +0530

    some_comments

--- Sorting/selection_sort.cpp
+++ Sorting/selection_sort.cpp
@@ -11,22 +11,24 @@ using namespace std;
 void selectionsort(int arr[],int n){
   for (int i = 0; i < n-1; i++)
   {
-    int min=i;
+
+    int min=i;        //for initial index
     for (int j = i+1; j < n; j++)
     {
       if (arr[i]>arr[j])
       {
-        min=j;
+        
+        min=j;      //update the initial value 
       }
      
       
     }
-     swap(arr[min],arr[i]);
+     swap(arr[min],arr[i]);          //swap the value
     
   }
   for (int i = 0; i < n; i++)
   {
-    cout<<arr[i]<<",";
+    cout<<arr[i]<<",";                 //print one by one value 
   }
   
   
@@ -36,7 +38,8 @@ void selectionsort(int arr[],int n){
 
 int main(){
   int one[6]={2,4,3,5,7,1};
-  selectionsort(one,6);
+  selectionsort(one,6);//call the function 
+
 
 
 

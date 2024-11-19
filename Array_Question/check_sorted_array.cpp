c9e36bc3dcd0304d74c2acecd49549e02b018f23
themanjeetkr <themanjeetkr@gmail.com>
2024-11-19 08:43:16 +0530

    check_sorted_array.cpp

---
+++ Array_Question/check_sorted_array.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+bool sorted(int arr[],int n){
+        int count=0;
+        for (int i = 1; i <n; i++)
+        {
+            if (arr[i-1]>arr[i])
+            {
+              count++;
+            }
+            
+            
+        }
+        if (arr[n-1]>arr[0])
+        {
+           count++;
+        }
+        return count<=1;
+        
+        
+
+
+}
+
+
+
+int main(){
+    int arr[5]={3,4,5,1,2};
+   int check= sorted(arr,5);
+
+   if (check)
+   {
+    cout<<"the array is sorted ";
+   }
+   else{
+    cout<<"not sorted ";
+   }
+   
+    
+
+}

469dce57660c384638ef10585618ff3c7621a000
themanjeetkr <themanjeetkr@gmail.com>
2024-11-28 20:18:49 +0530

    array_address

---
+++ _Pointers/array_address.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[10]={2,4,6};
+    cout<<"the address of first memory block is "<<&arr[0]<<endl;
+
+    cout<<"4th"<<*arr<<endl;
+        // arr[2]=*arr+1 or arr[i]=*(arr+i) or i[arr]=*(i+arr)
+    cout<<"5th"<<*arr+1<<endl;
+    cout<<"6th"<<*(arr+1)<<endl; 
+    cout<<"7th"<<*(arr)+1 <<endl;
+}

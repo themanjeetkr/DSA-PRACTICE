232ed3d3efd27b2b7a15978c8101249a323d822e
themanjeetkr <themanjeetkr@gmail.com>
2024-12-16 10:59:37 +0530

    some

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 void bubble_sort(int arr[],int n){
-    for (int i = 1; i < n-1; i++)
+    for (int i = 1; i < n-1; i++)// starting from index 1
     {
-       for (int j = 0; j < n-i; j++)
+       for (int j = 0; j < n-i; j++) // compare with index 0
        {
-        if (arr[j]>arr[j+1])
+        if (arr[j]>arr[j+1]) //compare index 1 with index 0
         {
             swap(arr[j],arr[j+1]);
         }

6739b530421d3f35472fa9341bfdb6bdf0ea8c2f
themanjeetkr <themanjeetkr@gmail.com>
2025-01-06 21:33:50 +0530

    factorial

---
+++ Recursion/factorial_no.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int factorial(int n){
+    if(n==0)
+       return 1;
+
+    int smallerproblem=factorial(n-1);
+    int biggerproblem=n*smallerproblem;
+    return biggerproblem;   
+}
+
+
+int main(){
+int n;
+cin>>n;
+int ans=factorial(n);
+cout<<ans;
+
+
+}

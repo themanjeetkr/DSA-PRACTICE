c84bb2395fb3f090d989d99ee3ee1aed01303242
themanjeetkr <themanjeetkr@gmail.com>
2025-01-06 11:13:43 +0530

    keywords

---
+++ IMP_Keywords/global_variable.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+int score=10;
+
+int main(){
+    cout<<score <<endl;
+    score++;
+    cout<<score;
+
+}
---
+++ IMP_Keywords/macro.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+#define pi 3.14
+ 
+int main(){
+    int r=5;
+    int area=pi*r*r;
+    cout<<area;
+    
+}

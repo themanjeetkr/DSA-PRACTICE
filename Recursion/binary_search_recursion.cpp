4d2fe83d5d3b0b5e6ee9585c7b759932b26894f1
themanjeetkr <themanjeetkr@gmail.com>
2025-01-08 10:44:22 +0530

    binary_search_recursion

---
+++ Recursion/binary_search_recursion.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+bool issorted(int arr[] ,int size ){
+    if(size==0||size==1)
+    return true;
+    if(arr[0]>arr[1])
+    return false;
+    else{
+        bool remainingpart=issorted(arr+1,size-1);
+        return remainingpart;
+
+    }
+
+
+}
+int main(){
+    int arr[5]={2,4,7,6,7};
+    int size=5;
+    bool ans =issorted(arr,size);
+    if(ans){
+        cout<<"array is sorted";
+    }
+    else{
+        cout<<"not sorted array";
+    }
+}

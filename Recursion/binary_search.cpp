d1c9f66e611ba138894aa56e40d5f8a0b7e85947
themanjeetkr <themanjeetkr@gmail.com>
2025-01-09 21:00:12 +0530

    binary_search

---
+++ Recursion/binary_search.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+bool binarysearch(int *arr, int s, int e, int k) {
+    if (s > e)
+        return false;
+    
+    int mid = s + (e - s) / 2;
+    
+    if (arr[mid] == k)
+        return true;
+    
+    if (arr[mid] < k) {
+       bool right = binarysearch(arr, mid + 1, e, k);
+        return right;
+    } else {
+        bool left = binarysearch(arr, s, mid - 1, k);          //return binarysearch(arr, s, mid - 1, k);
+        return left; 
+    }
+}
+
+int main() {
+    int arr[] = {2, 3, 4, 5, 5};
+    int size = 5;
+    int key = 2;
+
+    if (binarysearch(arr, 0, size - 1, key))
+        cout << "Key found" << endl;
+    else
+        cout << "Key not found" << endl;
+
+    return 0;
+}

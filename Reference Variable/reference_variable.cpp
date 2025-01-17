09fdf59e273d2d44c462ff08cb6fd052b455977a
themanjeetkr <themanjeetkr@gmail.com>
2025-01-17 19:29:06 +0530

    comit

--- Reference Variable/reference_variable.cpp
+++ Reference Variable/reference_variable.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
  int main(){
      int i=5;
-     int &j=i;        //&j used for reference value 
+     int &j=i;        //&j used for reference value //reference means new name same memory
      cout<<i<<endl;     //before
      i++;          
       cout<<i<<endl;// 

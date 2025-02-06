5dc6d1a7a4ba881c8283163debc93ffff270a344
themanjeetkr <themanjeetkr@gmail.com>
2025-02-06 08:46:09 +0530

    intro

--- (A.A)_OOPS/Day_1/intro.cpp
+++ (A.A)_OOPS/Day_1/intro.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 using namespace std;
 class hero{
+    //data members of clas
     int health;
 };
 int main(){
+    //creation of object
     hero h1;
     cout<<"size"<<sizeof(h1);
 }

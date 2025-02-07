f5581ccb50cc448fd5bb9d79b4e340cf0113a7b7
themanjeetkr <themanjeetkr@gmail.com>
2025-02-07 09:20:15 +0530

    copy_constrructor

---
+++ (A.A)_OOPS/Day_1/copy_construct.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+
+class hero {
+
+private:
+    int health;
+
+public:
+    char level;
+    //copy constructor
+    hero(hero &temp){
+        this->health=temp.health;
+        this->level=temp.level;
+    }
+
+    //constructor
+    hero(int health,char level){
+        this->health=health;
+        cout<<"constructor called"<<endl;
+        this->level=level;
+
+    }
+    hero(){
+        cout<<"simple constructor"<<endl;
+
+    }
+ 
+    int gethealth() {
+        return health;
+    }
+
+    char getlevel() {  // Fix: Changed return type to char
+        return level;
+    }
+
+    void sethealth(int h) {
+        health = h;
+    }
+
+    void setlevel(char ch) {
+        level = ch;
+    }
+    void print(){
+        cout<<"the health is :"<<this->health<<endl;
+        cout<<"the level is :"<<this->level<<endl;
+    }
+};
+int main(){
+//     hero suresh(10,'a');
+// //     suresh.sethealth(10);
+// //     suresh.setlevel('a');
+//    cout<<"the suresh's health :"<<suresh.gethealth()<<endl;
+//    cout<<"the level of suresh :"<<suresh.getlevel();
+   hero s(20,'c');
+   s.print(); 
+   hero r(s);
+   r.print();
+
+ }

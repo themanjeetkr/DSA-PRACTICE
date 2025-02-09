5064f55a667774e812c06fe3b2e8c5b686279bd7
themanjeetkr <themanjeetkr@gmail.com>
2025-02-09 12:12:49 +0530

    into

---
+++ (B)_Linked list/intro1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node*next;
+    node(int value){
+        this->data=value;
+        this->next=NULL;
+
+    }
+};
+
+int main(){
+
+    // creation of new node
+    node* node1=new node(10);
+    cout<<node1->data<<endl;
+    cout<<node1->next<<endl;
+
+}

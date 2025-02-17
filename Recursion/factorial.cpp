1a7099961cfed0211de9fb712fd3834d811c10f8
themanjeetkr <themanjeetkr@gmail.com>
2025-02-17 19:26:41 +0530

    factorial

---
+++ Recursion/factorial.cpp
@@ -0,0 +1,18 @@
+    #include<iostream>
+    using namespace std;
+    int fibonnaci(int fib){
+        if(fib==1|| fib==0){
+            return 1;
+
+        }
+        int badanumber= fib*fibonnaci(fib-1);
+        return badanumber;
+        
+    }
+
+    int main(){
+        int num=10;
+        cout<<"the factorial is "<<fibonnaci(num);
+
+
+    }

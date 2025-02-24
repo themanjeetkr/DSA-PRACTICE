78b5db3fdd32e7a70eaf9351c0e7db8c14b04582
themanjeetkr <themanjeetkr@gmail.com>
2025-02-24 19:39:58 +0530

    creation

---
+++ (C)_Stack/creation.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+    // creation of stack
+    stack<int>s;
+// PUSH THE VALUE
+    s.push(2);
+    s.push(3);
+    //POP
+    s.pop();
+
+    cout<<"printing top element"<<s.top()<<endl;
+    if (s.empty())
+    {
+        /* code */
+        cout<<"stack is empty "<<endl;
+    }
+    else{
+        cout<<"stack is not empty"<<endl;
+    }
+    
+
+
+}

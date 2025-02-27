4ffbe8aac33c8dfc6f38ec7f7d775327b49380f0
themanjeetkr <themanjeetkr@gmail.com>
2025-02-27 20:15:43 +0530

    reverse

---
+++ (C)_Stack/reverse.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+    string str="themanjeetkr";
+    stack<char>s;
+    for (int  i = 0; i < str.length(); i++)
+    {
+    char ch=str[i];
+    s.push(ch);
+    }
+    string ans=" ";
+    while (!s.empty())
+    {
+        /* code */
+        char ch=s.top();
+        ans.push_back(ch);
+        s.pop();
+    }
+    cout<<"the answer is "<<ans<<endl;
+    
+    
+}

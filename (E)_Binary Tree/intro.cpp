bde8d6685329723fc19047142ad2cafed95f5812
themanjeetkr <themanjeetkr@gmail.com>
2025-03-17 22:46:37 +0530

    temp_fixed

--- (E)_Binary Tree/intro.cpp
+++ (E)_Binary Tree/intro.cpp
@@ -77,19 +77,48 @@ void postorder(node *root){
     cout<<root->data;
 }
 node* buildfromlevelorder(node* root){
+    queue<node*>q;
+    cout<<"enter data for root "<<endl;
+    int data;
+    cin>>data;
+    root=new node(data);
+    q.push(root);
+    while (!q.empty())
+    {
+        /* code */
+        node*temp=q.front();
+        q.pop();
+        cout<<"enter the left node for "<<temp->data<<endl;
+        int leftdata;
+        cin>>leftdata;
+        if(leftdata!=-1){
+            temp->left=new node(leftdata);
+            // push temp
+            q.push(temp->left);
+        }
+        cout<<"enter the right node for "<<temp->data<<endl;
+        int right;
+        cin>>right;
+        if(right!=-1){
+            temp->right=new node(leftdata);
+            q.push(temp->right);
+        }
+    }
+    
 
 }
 
 int main() {
     node* root = NULL;
-    root = buildtree(root);
-    cout << "Level Order Traversal: ";
-    levelorder(root);
+    // root = buildtree(root);
+    // cout << "Level Order Traversal: ";
+    // levelorder(root);
 
-    cout << "Preorder Traversal: "<<endl;
-    preorder(root);
-    cout<<"postorder Traversal"<<endl;
-    postorder(root);
+    // cout << "Preorder Traversal: "<<endl;
+    // preorder(root);
+    // cout<<"postorder Traversal"<<endl;
+    // postorder(root);
 
-    return 0;
+    // return 0;
+    buildfromlevelorder(root);
 }
